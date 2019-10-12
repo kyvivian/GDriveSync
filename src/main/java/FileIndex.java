73010bf2926781469a8f961cf78c824bf8df41b0
kyvivian <vivianky0713@gmail.com>
2019-10-12 10:37:59 -0700

    able to list files. next createIndex

--- src/main/java/FileIndex.java
+++ src/main/java/FileIndex.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.IOException;
 
 public class FileIndex {
     private File root_dir;
@@ -7,23 +8,57 @@ public class FileIndex {
         this.root_dir = new File(root_dir);
     }
 
-    /* Creates a hidden index file that stores all the  
-     * files/dirs of the current directory
+    /*
+     * Creates a hidden index file that stores all the files/dirs of the current
+     * directory
      * 
-     * Parameters:
-     *    cur_dir = directory that we want to index
+     * Parameters: cur_dir = directory that we want to index
      * 
-     * Returns:
-     *    -1 if cur_dir is not a directory or there already exists ".index.txt"
-     *     1 if ".index.txt" is successfully created
+     * Returns: false if cur_dir is not a directory or there already exists
+     * ".index.txt" true if ".index.txt" is successfully created
      */
-    public int createIndex(File cur_dir){
+    public boolean createIndex(File cur_dir) throws IOException {
         if(!cur_dir.isDirectory()) {
-            return -1;
+            return false;
         }
         File index = new File(cur_dir, ".index.txt");
 
-        return 1;
+        return index.createNewFile();
+    }
+    
+    public void listFiles() {
+        if(!root_dir.isDirectory()) {
+            System.out.println("root is not a directory");
+        }
+
+        String[] fileList = root_dir.list();
+
+        for (String a : fileList) {
+            System.out.println(a);
+        }
+    }
+
+    public void syncIndex(File syncFolder) {
+        String[] fileList = syncFolder.list();
+
+        for(String f : fileList) {
+            File cur_file = new File(syncFolder, f);
+
+            if(cur_file.isDirectory()) {
+                //System.out.println(cur_file.getName() + " is a directory");
+                syncIndex(cur_file);
+            }
+            else {
+                //System.out.println(cur_file.getName() + " is a file");
+                System.out.println(cur_file.getName());
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        FileIndex ind = new FileIndex("C:\\Users\\Vivian Ky\\Desktop\\X1");
+        ind.syncIndex(new File("C:\\Users\\Vivian Ky\\Desktop\\X1\\3+"));
     }
 
 }
