f2f232dc24417edc14b6cb465ad34fd56b81e77b
kyvivian <vivianky0713@gmail.com>
2019-10-16 11:34:19 -0700

    hiding files

--- src/main/java/FileIndex.java
+++ src/main/java/FileIndex.java
@@ -1,5 +1,9 @@
 import java.io.File;
 import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 public class FileIndex {
     private File root_dir;
@@ -18,12 +22,43 @@ public class FileIndex {
      * ".index.txt" true if ".index.txt" is successfully created
      */
     public boolean createIndex(File cur_dir) throws IOException {
+        System.out.println("Creating index for " + cur_dir.getPath() + "\n");
         if(!cur_dir.isDirectory()) {
             return false;
         }
         File index = new File(cur_dir, ".index.txt");
 
-        return index.createNewFile();
+        //get the index list 
+        String[] fileList = root_dir.list();
+
+        //create the file .index.txt
+        boolean createdFile = index.createNewFile();
+
+        if(!createdFile) {
+            return false;
+        }
+
+        Writer writeFile = null;
+
+        //write to .index.txt and then close the file
+        try {
+            writeFile = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(index.getPath()), "utf-8"));
+            for (String a: fileList) {
+                writeFile.write(a + "\n");
+            }
+        }
+        catch (IOException ex) {
+            System.out.println("failed to write to index");
+            return false;
+        }
+        finally {
+            try {
+                writeFile.close();
+            }
+        catch (Exception ex) {System.out.println("couldn't close index properly");}
+        }
+        return true;
+        
     }
     
     public void listFiles() {
@@ -56,9 +91,11 @@ public class FileIndex {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        File root = new File("C:\\Users\\Vivian Ky\\Desktop\\X1");
         FileIndex ind = new FileIndex("C:\\Users\\Vivian Ky\\Desktop\\X1");
-        ind.syncIndex(new File("C:\\Users\\Vivian Ky\\Desktop\\X1\\3+"));
+        //ind.syncIndex(new File("C:\\Users\\Vivian Ky\\Desktop\\X1\\3+"));
+        ind.createIndex(root);
     }
 
 }
